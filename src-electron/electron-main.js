be7c0ef09f6a5bea31546f39e42cf48eba5c8c5c
Alan Xu <xkevas@qq.com>
2022-03-04 19:44:44 +0800

    test macos

--- src-electron/electron-main.js
+++ src-electron/electron-main.js
@@ -15,110 +15,6 @@ try {
 catch (_) { }
 
 const isMac = process.platform === 'darwin'
-const template = [
-  // { role: 'appMenu' }
-  ...(isMac ? [{
-    label: app.name,
-    submenu: [
-      { role: 'about' },
-      { type: 'separator' },
-      { role: 'services' },
-      { type: 'separator' },
-      { role: 'hide' },
-      { role: 'hideOthers' },
-      { role: 'unhide' },
-      { type: 'separator' },
-      { role: 'quit' }
-    ]
-  }] : [{
-    label: "This is Main Window!"
-  }]),
-  // { role: 'fileMenu' }
-  {
-    label: '文件',
-    submenu: [
-      isMac ? { role: 'close' } : { role: 'quit' }
-    ]
-  },
-  // { role: 'editMenu' }
-  {
-    label: 'Edit',
-    submenu: [
-      { role: 'undo' },
-      { role: 'redo' },
-      { type: 'separator' },
-      { role: 'cut' },
-      { role: 'copy' },
-      { role: 'paste' },
-      ...(isMac ? [
-        { role: 'pasteAndMatchStyle' },
-        { role: 'delete' },
-        { role: 'selectAll' },
-        { type: 'separator' },
-        {
-          label: 'Tool',
-          submenu: [
-            { role: 'startSpeaking' },
-            { role: 'stopSpeaking' }
-          ]
-        },
-      ] : [
-        { role: 'delete' },
-        { type: 'separator' },
-        { role: 'selectAll' }
-      ])
-    ]
-  },
-  // { role: 'viewMenu' }
-  {
-    label: 'View',
-    submenu: [
-      { role: 'reload' },
-      { role: 'forceReload' },
-      { role: 'toggleDevTools' },
-      { type: 'separator' },
-      { role: 'resetZoom' },
-      { role: 'zoomIn' },
-      { role: 'zoomOut' },
-      { type: 'separator' },
-      { role: 'togglefullscreen' }
-    ]
-  },
-  // { role: 'windowMenu' }
-  {
-    label: 'Window',
-    submenu: [
-      { role: 'minimize' },
-      { role: 'zoom' },
-      ...(isMac ? [
-        { type: 'separator' },
-        { role: 'front' },
-        { type: 'separator' },
-        { role: 'window' },
-      ] : [
-        { role: 'close' }
-      ])
-    ]
-  },
-  {
-    role: 'help',
-    submenu: [
-      {
-        label: 'Learn More',
-        click: async () => {
-          const { shell } = require('electron')
-          await shell.openExternal('https://electronjs.org')
-        }
-      }
-    ]
-  },
-  {
-    label: 'Preview Only',
-    click: async () => {
-      subWindow.webContents.send("smp","HIDE SOURCE");
-    }
-  }
-]
 let MainMenus = [
   {
     label:'This is Main Window!',
@@ -145,7 +41,6 @@ let MainMenus = [
     }
   },
 ];
-
 
 const SubMenus = [
   {
@@ -205,7 +100,7 @@ function createWindow () {
   })
 
   // Menu.setApplicationMenu(Menu.buildFromTemplate(MainMenus));
-  mainWindow.setMenu(Menu.buildFromTemplate(template))
+  mainWindow.setMenu(Menu.buildFromTemplate(MainMenus))
 }
 
 function createPreview () {
